42e593fc58b974b63e8caeb059ed6b9dfee6f068
Chan Chris <miemiekurisu@aol.com>
2015-12-03 23:50:31 +0800

    d

--- test/testbot.py
+++ test/testbot.py
@@ -15,19 +15,17 @@ token = cfg.get('token','token')
 print token
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
 @bot.message_handler(commands=['googefor'])
 def search_test(message):
     markup = types.ForceReply(selective=False)
     bot.reply_to(message.getMessageID(),'send me words to search:', reply_markup=markup)
     
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
     
 if __name__ == '__main__':
     bot.polling()
